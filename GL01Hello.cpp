6879fb16e93fdbd41e7be21dfa83b66622cfbf96
Colby Sullivan <88957296+colbySullivan@users.noreply.github.com>
2023-06-22 12:05:58 -0500

    All inputs running

--- GL01Hello.cpp
+++ GL01Hello.cpp
@@ -1,60 +1,130 @@
-/*
- * GL01Hello.cpp
- *
- *  Created on: Jun 21, 2023
- *  Author: ColbySullivan
- */
+#include <GL/glut.h>
+#include <GL/glut.h>
+#include <cstdlib>
 
-#include <windows.h>  // For MS Windows
-#include <GL/glut.h>  // GLUT, includes glu.h and gl.h
+static bool spinning = true;
+static const int FPS = 60; // This is the number of frames per second to render.
+static GLfloat currentAngleOfRotation = 0.0;
+const float window_height = 500;
+const float window_width = 500;
 
-/* Handler for window-repaint event. Call back when the window first appears and
-   whenever the window needs to be re-painted. */
+// Performs application-specific initialization. Sets colors and sets up a
+// simple orthographic projection.
+void init() {
 
-#define WINDOW_WIDTH 320
-#define WINDOW_HEIGHT 240
+  // Set a deep purple background and draw in a greenish yellow.
+  glClearColor(0.5, 0.0, 0.2, 1.0);
+  glColor3f(0.7, 1.0, 0.0);
 
-void display();
-void init();
-
-int main(int argc, char* argv[])
-{
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-    glutCreateWindow("Main Window");
-    init(); // changed the init function to come directly before display function
-    glutDisplayFunc(display);
-    glutMainLoop();
-
-    return 0;
+  // Set up the viewing volume: 500 x 500 x 1 window with origin lower left.
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glOrtho(0.0, 500.0, 0.0, 500.0, 0.0, 1.0);
 }
 
-void init()
-{
-    glClearColor(0, 0, 0, 0); // moved this line to be in the init function
-    glDisable(GL_DEPTH_TEST);
-
-    // next four lines are new
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(0.0, WINDOW_WIDTH-1, WINDOW_HEIGHT-1, 0, -1.0, 1.0);
-    glMatrixMode(GL_MODELVIEW);
+void timer(int v) {
+  if (spinning) {
+    currentAngleOfRotation += 1.0;
+    if (currentAngleOfRotation > 360.0) {
+      currentAngleOfRotation -= 360.0;
+    }
+    glutPostRedisplay();
+  }
+  glutTimerFunc(1000/FPS, timer, v);
 }
 
-void display()
-{
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+// Handles mouse events
+void mouse(int button, int state, int x, int y) {
+  if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+    spinning = true;
+  } else if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
+    spinning = false;
+  }
+}
+
+void drawBitmapText(char* text, int x, int y) {
+    char* c;
+
     glLoadIdentity();
+    glColor3f(0, 0, 0);
+    glRasterPos2f(x, y);
 
-    glBegin(GL_QUADS);
-    glColor3ub(255,255,255); // changed glColor3i to glColor3ub (see below)
-        glVertex2f(10, 10);
-        glVertex2f(100, 10);
-        glVertex2f(100, 100);
-        glVertex2f(10, 100);
-    glEnd();
+    for(c = text; *c != '\0'; c++){
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, *c);
+    }
+}
 
-    glFlush(); // added this line
-    //glutSwapBuffers(); // removed this line
+void drawControls(){
+    char* move_message = (char*) "Use S to start rotation and SPACE to stop";
+    char* exit_message = (char*) "Press ESC or Q to exit";
+
+    drawBitmapText(move_message, -70, 40);
+    drawBitmapText(exit_message, 10, -40);
+    glFlush();
+    glutSwapBuffers();
+}
+
+void drawTriangle(){
+	glClear(GL_COLOR_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glRotatef(currentAngleOfRotation, 0.0, 0.0, 1.0);
+	glRectf(-25.0, -25.0, 25.0, 25.0); //Dimensions of object
+	glFlush();
+	glutSwapBuffers();
+}
+
+void display() {
+
+  glClear(GL_COLOR_BUFFER_BIT);
+  glRasterPos2f(250, 250);
+  drawTriangle();
+  drawControls();
+}
+
+void reshape(GLint w, GLint h) {
+  glViewport(0, 0, w, h);
+  GLfloat aspect = (GLfloat)w / (GLfloat)h;
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  if (w <= h) {
+    // width is smaller, go from -50 .. 50 in width
+    glOrtho(-80.0, 80.0, -50.0/aspect, 50.0/aspect, -1.0, 1.0);
+  } else {
+    // height is smaller, go from -50 .. 50 in height
+    glOrtho(-80.0*aspect, 50.0*aspect, -50.0, 50.0, -1.0, 1.0);
+  }
+}
+
+void keyboard(unsigned char key, int x, int y){//Need unused x and y for glutKeyboardFunc
+	switch(key){
+    	case 's':{
+        	spinning = true;
+            break;
+        }
+        case ' ':{
+        	spinning = false;
+            break;
+        }
+        case 27:// Escape key
+        case 'q':{
+            exit(0);
+        }
+    }
+}
+// Initializes GLUT, the display mode, and main window; registers callbacks;
+// does application initialization; enters the main event loop.
+int main(int argc, char** argv) {
+  glutInit(&argc, argv);
+  glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+  glutInitWindowSize(window_height, window_width);
+  glutInitWindowPosition(40, 40);
+  glutCreateWindow("Decision Wheel");
+  glutDisplayFunc(display);
+  glutReshapeFunc(reshape);
+  glutTimerFunc(100, timer, 0);
+  glutMouseFunc(mouse);
+  glutKeyboardFunc(keyboard);
+  init();
+  glutMainLoop();
 }
